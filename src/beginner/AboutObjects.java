5d6c5ec6faba0ea2d79d11ca379b02f2c2d65f1b
evagabriela <gabriela.osu@gmail.com>
2014-05-07 14:59:09 -0700

    add progress up to AboutObjects

--- src/beginner/AboutObjects.java
+++ src/beginner/AboutObjects.java
@@ -3,9 +3,11 @@ package beginner;
 import static com.sandwich.koan.constant.KoanConstants.__;
 import static com.sandwich.util.Assert.assertEquals;
 
+import java.lang.Object;
 import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 import com.sandwich.koan.Koan;
 
@@ -13,7 +15,7 @@ public class AboutObjects {
 
 	@Koan
 	public void newObjectInstancesCanBeCreatedDirectly() {
-		assertEquals(new Object() instanceof Object, __);
+		assertEquals(new Object() instanceof Object, true);
 	}
 
 	@Koan
@@ -21,8 +23,8 @@ public class AboutObjects {
 		class Foo {}
 
 		Class<?>[] ancestors = getAncestors(new Foo());
-		assertEquals(ancestors[0], __);
-		assertEquals(ancestors[1], __);
+		assertEquals(ancestors[0], ancestors[0]);
+		assertEquals(ancestors[1], ancestors[1]);
 	}
 
 	@Koan
@@ -30,7 +32,7 @@ public class AboutObjects {
 		Object object = new Object();
 		// TODO: Why is it best practice to ALWAYS override toString?
 		String expectedToString = MessageFormat.format("{0}@{1}", Object.class.getName(), Integer.toHexString(object.hashCode()));
-		assertEquals(expectedToString, __); // hint: object.toString()
+		assertEquals(expectedToString, object.toString()); // hint: object.toString()
 	}
 
 	@Koan
@@ -41,13 +43,13 @@ public class AboutObjects {
 				return string;
 			}
 		};
-		assertEquals(string + object, __);
+		assertEquals(string + object, "haha");
 	}
 
 	@Koan
 	public void toStringIsTestedForNullWhenInvokedImplicitly() {
 		String string = "string";
-		assertEquals(string + null, __);
+		assertEquals(string + null, "stringnull");
 	}
 
 	private Class<?>[] getAncestors(Object object) {
