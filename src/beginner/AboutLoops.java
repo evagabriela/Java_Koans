20ac8df8cf70eb731a78fddb3372b35b0f2a118a
evagabriela <gabriela.osu@gmail.com>
2014-05-07 16:00:49 -0700

    add progress up to AboutLoops

--- src/beginner/AboutLoops.java
+++ src/beginner/AboutLoops.java
@@ -14,7 +14,7 @@ public class AboutLoops {
 		for(int i = 0; i < 5; i++) {
 			s += i + " ";
 		}
-		assertEquals(s, __);
+		assertEquals(s, "0 1 2 3 4 ");
 	}
 	
 	@Koan
@@ -23,7 +23,7 @@ public class AboutLoops {
 		for(int i = 0, j = 10; i < 5 && j > 5; i++, j--) {
 			s += i + " " + j + " ";
 		}
-		assertEquals(s, __);
+		assertEquals(s, "0 10 1 9 2 8 3 7 4 6 ");
 	}
 	
 	@Koan
@@ -33,7 +33,7 @@ public class AboutLoops {
 		for(int j : is) {
 			s += "." + j;
 		}
-		assertEquals(s, __);
+		assertEquals(s, "-.1.2.3.4");
 	}
 	
 	@Koan
@@ -42,7 +42,7 @@ public class AboutLoops {
 		while(result < 3) {
 			result++;
 		}
-		assertEquals(result, __);
+		assertEquals(result, 3);
 	}
 	
 	@Koan
@@ -51,7 +51,7 @@ public class AboutLoops {
 		do {
 			result++;
 		} while(false);
-		assertEquals(result, __);
+		assertEquals(result, 1);
 	}
 	
 	@Koan
@@ -64,7 +64,7 @@ public class AboutLoops {
 			}
 			count++;	
 		}
-		assertEquals(count, __);
+		assertEquals(count, 1);
 	}
 	
 	@Koan
@@ -78,7 +78,7 @@ public class AboutLoops {
 				count++;	
 			}
 		}
-		assertEquals(count, __);
+		assertEquals(count, 2);
 	}
 
 	@Koan
@@ -95,9 +95,12 @@ public class AboutLoops {
 			}
 			count += 10;
 		}
-		// What does continue with a label mean? 
+		// What does continue with a label mean?
+            // ANSWER: its the name that you are giving to the whole block of code that goes after it until its a break statement
+
 		// What gets executed? Where does the program flow continue?
-		assertEquals(count, __);
+            // ?? what this question means?
+		assertEquals(count, 7);
 	}
 	
 	@Koan
@@ -116,7 +119,7 @@ public class AboutLoops {
 		}
 		// What does break with a label mean? 
 		// What gets executed? Where does the program flow continue?
-		assertEquals(count, __);
+		assertEquals(count, 3);
 	}
 
 }
